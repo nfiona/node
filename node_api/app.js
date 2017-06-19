14d91ef805f9dfadc54469978c69f12337d750f3
fiona <h.natapra@gmail.com>
2017-06-18 22:37:36 -0400

    created remote MongoDB (MLab), installed mongoose and connected to DB

--- node_api/app.js
+++ node_api/app.js
@@ -7,6 +7,10 @@ var server = restify.createServer();
 var setupController = require('./controllers/setupController.js');
 var userController = require('./controllers/userController.js');
 var restifyValidator = require('restify-validator');
+var config = require("./config/dbConnections.js")
+// connecting to MongoDB (MLab) with mongoose.
+var mongoose = require('mongoose');
+mongoose.connect(config.getMongoConnection());
 // *** Define functions from the above external files & plugins
 setupController(server, restify, restifyValidator);
 userController(server);
---
+++ node_api/config/dbConnections.js
@@ -0,0 +1,6 @@
+// *** Connect to MLab, insert MongoDB URI
+module.exports = {
+  getMongoConnection: function() {
+    return 'mongodb://testuser:testTest@ds131492.mlab.com:31492/nodesample_api';
+  }
+}
