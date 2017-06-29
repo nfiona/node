058a8445f5fede3ac76b35f677564bb97edf94b8
fiona <h.natapra@gmail.com>
2017-06-29 12:59:24 -0400

    API Keys with Basic Auth w/ Restify

--- node_api/controllers/setupController.js
+++ node_api/controllers/setupController.js
@@ -9,5 +9,26 @@ module.exports = function(server, restify, restifyValidator) {
   server.use(restify.queryParser());
   // *** restify-validator plugin.
   server.use(restifyValidator);
-
+  // Parse HTTP Basic Access Authentication for {'username': 'password'}
+  server.use(restify.authorizationParser());
+  // Use anonymous function for error handling
+  server.use(function(req, res, next) {
+    var apiKeys = {
+      "user1": "ahsdh767aksjdbkjsabd67asd"
+    };
+    // Set condition: if authorization returns undefined, OR if username doesn't exit in the apiKeys array OR if password doesn't match the username stored in the apiKeys array,
+    // then, send error message. Otherwise, proceed.
+    if (typeof(req.authorization.basic) === 'undefined' || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.username]) {
+      var response = {
+        "status": "failure",
+        "data": "Must specify a valid API Key."
+      }
+      // Borrow from helper funtion, sending our responses
+      res.setHeader('content-type','application/json');
+      res.writeHead(http_code); // forbidden error code.
+      res.end(JSON.stringify(response));
+      // return next();
+    }
+    return next();
+  })
 }
